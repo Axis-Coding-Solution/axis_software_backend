d2f58728f117b18fe50f9b21d57fb44069864d3a
nomi-14-github <samdhekamdhe17@gmail.com>
2024-10-22 17:29:00 +0500

    db establish command and uninstall useles dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'this is text';
+export class AppService implements OnModuleInit {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+  async onModuleInit() {
+    try {
+      const isConnected = this.connection.readyState === 1;
+      if (isConnected) {
+        console.log('Database connection established');
+      } else {
+        console.log('Database connection not established');
+      }
+    } catch (error) {
+      console.error('Error checking database connection:', error);
+    }
   }
 }
