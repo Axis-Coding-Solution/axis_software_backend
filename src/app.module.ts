d667a5399535e45c7a823360bceabf50eae2821f
nouman <samdhekamdhe17@gmail.com>
2025-02-03 19:19:28 +0500

    get all user api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DataBaseModule } from './infra/mongoose/database.module';
-import { UserModule } from './controllers/auth/auth.module';
+import { AuthModule } from './controllers/auth/auth.module';
 import { DepartmentModule } from './controllers/employees/department/department.module';
 import { DesignationModule } from './controllers/employees/designation/designation.module';
 import { CheckDbConnectionCommand } from './command';
@@ -11,17 +11,19 @@ import { CompanyModule } from './controllers/commons/company/company.module';
 import { UploadFileInterceptor } from './middlewares';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { EmployeeModule } from './controllers/employees/employee/employee.module';
+import { UserModule } from './controllers/user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     DataBaseModule,
-    UserModule,
+    AuthModule,
     DepartmentModule,
     DesignationModule,
     CompanyModule,
     EmployeeModule,
+    UserModule,
     ServeStaticModule.forRoot({
       rootPath: `${process.cwd()}/uploads`,
       serveRoot: '/uploads',
--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-
+import { successfulResponse } from 'src/util';
+import { isAdminGuard, JwtAuthGuard } from 'src/middlewares/guard';
+@UseGuards(JwtAuthGuard, isAdminGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAll() {
+    const users = await this.userService.getAll();
+    return successfulResponse('Users found successfully', users);
+  }
 }
