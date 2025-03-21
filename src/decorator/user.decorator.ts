7eab8e12b80de90afe0c73cafa8013425f472cf3
nouman <samdhekamdhe17@gmail.com>
2025-03-21 10:24:47 +0500

    correct param informatin

--- src/decorator/user.decorator.ts
+++ src/decorator/user.decorator.ts
@@ -1,9 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { UserPayloadInterface } from 'src/interfaces/user';
 /**
- * This decorator is used to get current user from the request
- * @param {String} data Role or id.If no data is passed then whole user object will be returned
- * @returns {String | UserPayloadInterface} User id or role or whole user object
+ * Custom decorator used to get current user from the request
+ * @param {keyof UserPayloadInterface | undefined} data Role or id.If no data is passed then whole user object will be returned.
+ * @param {ExecutionContext} ctx The execution context
+ * @returns {String | UserPayloadInterface} User id or role or whole user object.
  */
 export const User = createParamDecorator(
   (data: keyof UserPayloadInterface | undefined, ctx: ExecutionContext) => {
