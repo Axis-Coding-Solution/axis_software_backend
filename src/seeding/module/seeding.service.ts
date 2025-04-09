802f53dfa88ce99ce8dc6540721d65699c0f5ad4
nouman <samdhekamdhe17@gmail.com>
2025-04-09 12:19:41 +0500

    make json data separate and import in query

--- src/seeding/module/seeding.service.ts
+++ src/seeding/module/seeding.service.ts
@@ -9,6 +9,7 @@ import {
 } from 'src/schemas/roles-and-permissions';
 import * as fs from 'fs';
 import * as path from 'path';
+import { groupData, menuData } from '../data';
 // import groupData from JSON.parse('../data/group.data.seeding.json');
 // const groupData = JSON.parse(fs.readFileSync(path:`${__dirname}/data/group.data.seeding.json`,options: 'utf-8'));
 // console.log("🚀 ~ groupData:", groupData)
@@ -25,78 +26,12 @@ export class SeedingService {
   async seedGroups() {
     await this.groupModel.deleteMany({});
 
-    await this.groupModel.insertMany([
-      {
-        role: 'admin',
-        description: 'Administrator with full access',
-      },
-      {
-        role: 'user',
-        description: 'Regular user with limited access',
-      },
-      {
-        role: 'employee',
-        description: 'Company employee with internal access',
-      },
-      {
-        role: 'client',
-        description: 'External client with specific access',
-      },
-    ]);
+    await this.groupModel.insertMany(groupData);
   }
 
   async seedMenus() {
     await this.menuModel.deleteMany({});
 
-    await this.menuModel.insertMany([
-      {
-        name: 'Employees',
-        path: '/employees',
-      },
-      {
-        name: 'Employees',
-        path: '/employees-list',
-      },
-      {
-        name: 'Holidays',
-        path: '/holidays',
-      },
-      {
-        name: 'Admin Leaves',
-        path: '/adminleaves',
-      },
-      {
-        name: 'Leaves Employee',
-        path: '/leaves-employee',
-      },
-      {
-        name: 'Leave Settings',
-        path: '/leave-settings',
-      },
-      {
-        name: 'Admin Attendance',
-        path: '/adminattendance',
-      },
-      {
-        name: 'Attendance Employee',
-        path: '/attendance-employee',
-      },
-      {
-        name: 'Departments',
-        path: '/departments',
-      },
-      {
-        name: 'Designations',
-        path: '/designations',
-      },
-      {
-        name: 'Timesheet',
-        path: '/timesheet',
-      },
-      {
-        name: 'Overtime',
-        path: '/overtime',
-      },
-    ]);
+    await this.menuModel.insertMany(menuData);
   }
 }
