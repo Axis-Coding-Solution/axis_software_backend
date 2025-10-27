f73e553095c82acee50fd68dff6064c6665d2171
nouman <samdhekamdhe17@gmail.com>
2025-10-27 12:03:41 +0500

    seprate logic for swagger exception filters comments for better readability

---
+++ src/swagger/swagger.config.ts
@@ -0,0 +1,8 @@
+import { SwaggerConfig } from './swagger.interface';
+
+export const SWAGGER_CONFIG: SwaggerConfig = {
+  setTitle: 'Axis Software Api',
+  setDescription: 'Axis Software Api',
+  setVersion: '1.0',
+  setBasePath: 'api',
+};
