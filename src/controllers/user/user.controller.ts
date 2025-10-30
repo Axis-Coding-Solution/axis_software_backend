8be798cfd34dec58be3fa603a7db885a1819d60a
nouman <samdhekamdhe17@gmail.com>
2025-10-30 14:06:52 +0500

    implementing cache

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
 import { successfulResponse } from 'src/utils';
 import { JwtAuthGuard, isAdminGuard } from '@/common/guards';
 import { USER_MODEL } from 'src/schemas/commons/user';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 @UseGuards(JwtAuthGuard, isAdminGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @UseInterceptors(CacheInterceptor)
   async getAll() {
     const users = await this.userService.getAll();
     return successfulResponse(`${USER_MODEL} found successfully`, users);
